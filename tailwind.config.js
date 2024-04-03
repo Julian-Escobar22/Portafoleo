0c20ec08160bc09dd02bafc200f7a6f6b45b6256
julian <julian.escobarf@campusucc.edu.cogit config --global user.email julian.escobarf@campusucc.edu.co>
2024-04-03 17:27:21 -0500

    segundo commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ export default {
       fontFamily:{
         roboto: ['Roboto', 'sans-serif'],
       },
+      colors:{
+        'app-portafoleo' : '#4ac2c4',
+        'app-primary':{
+          50: 'f4f5fe',
+          100: '#eaebfd',
+          200: '#d9dcfb',
+          300: '#babdf8',
+          400: '#9394f2',
+          500: '#6d67eb',
+          600: '#5747e0',
+          700: '#4935cc',
+          800: '#3c2cab',
+          900: '#352792',
+        }
+      },
     },
   },
   plugins: [],
