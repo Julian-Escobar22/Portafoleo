287f778895b28dbfe61fc27496c2c7041ae1ee4e
julian <julian.escobarf@ccampusucc.edu.co>
2024-05-13 18:15:26 -0500

    corrigiendo diseños

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app/App.jsx';
+import App from './App.jsx';
 import './assets/styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import './i18n.js';
